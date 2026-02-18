94cab569b432b8798c27d6c10e625fd994bcb096
erickaferraz <ferrazaerick@gmail.com>
2026-02-18 13:03:36 -0300

    ajustando conexão para Neon com SSL

--- database.py
+++ database.py
@@ -5,13 +5,17 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+import psycopg2
+import os
+
 def get_connection():
     return psycopg2.connect(
-        dbname=os.getenv("DB_NAME"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
         host=os.getenv("DB_HOST"),
-        port=os.getenv("DB_PORT")
+        database=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASS"),
+        port=os.getenv("DB_PORT"),
+        sslmode='require'  # <--- ESSENCIAL PARA O NEON
     )
 
 def init_db():
